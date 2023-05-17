d0e3eb278bec4f9021e8918f36b53ac84197b1a3
나종현 <i0364842@naver.com>
2023-05-17 14:46:11 +0900

    ing

--- frontend/src/components/tour/TourSearchInfo.vue
+++ frontend/src/components/tour/TourSearchInfo.vue
@@ -1,66 +1,142 @@
 <template>
-    <v-app id="">
-        <div style="display:flex">
-            <v-select
-                id="search-area"
-                label="시, 도"
-                :items="sido"
-                item-text="sidoName"
-                item-value="sidoCode"
-                v-model="selectedSido"
-                outlined
+<v-app id="inspire">
+    <v-main class="white">
+    <v-container class="text-center">
+    <h1>여행정보</h1>
+    <v-divider class="my-2"></v-divider>
+        <v-row style="margin-top: 10px;">
+            <!-- Left tour map position -->
+            <v-col cols="6">
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+                elevation="8"
+                class="lighten-5 overflow-auto"
+                style="padding: 10px;"
             >
-            </v-select>
-            <v-select
-                class="ms-4"
-                label="군, 구"
-                :items="gugun"
-                item-text="gugunName"
-                item-value="gugunCode"
-                v-model="selectedGugun"
-                outlined
-            ></v-select>
-        </div>
-        <div style="display:flex">
-            <v-select
-                class="me-2"
-                clearable
-                chips
-                label="여행지 종류"
-                :items="contentByType"
-                item-text="value"
-                item-value="id"
-                v-model="selectedContentById"
-                multiple
-                outlined
-            ></v-select>
-            <v-btn
-                @click="markList"
-                class="ma-2"
-                outlined
-                large
-                fab
-                color="indigo"
+                <div style="display:flex">
+                    <v-select
+                        id="search-area"
+                        label="시, 도"
+                        :items="sido"
+                        item-text="sidoName"
+                        item-value="sidoCode"
+                        v-model="selectedSido"
+                        outlined
+                    >
+                    </v-select>
+                    <v-select
+                        class="ms-4"
+                        label="군, 구"
+                        :items="gugun"
+                        item-text="gugunName"
+                        item-value="gugunCode"
+                        v-model="selectedGugun"
+                        outlined
+                    ></v-select>
+                </div>
+                <div style="display:flex">
+                    <v-select
+                        class="me-2"
+                        clearable
+                        chips
+                        label="여행지 종류"
+                        :items="contentByType"
+                        item-text="value"
+                        item-value="id"
+                        v-model="selectedContentById"
+                        multiple
+                        outlined
+                    ></v-select>
+                    <v-btn
+                        @click="markList"
+                        class="ma-2"
+                        outlined
+                        large
+                        fab
+                        color="indigo"
+                        >
+                        <v-icon
+                            large
+                            color="darken-2"
+                        >
+                        mdi-magnify
+                        </v-icon>
+                    </v-btn>
+                </div>
+                <div class="mt-3 me-3">
+                    <div id="map" class="" style="width: 100%; height: 700px;"></div>
+                </div>
+
+            </v-sheet>
+            </v-col>
+            <!-- Right tour List position -->
+            <v-col cols=6>
+            <v-sheet
+                min-height="85vh"
+                max-height="100"
+                rounded="lg"
+                elevation="8"
+                class="grey lighten-5 overflow-auto"
+                style="padding: 10px;"
+            >
+                <!--여기다가 card 추가-->
+                <v-container fluid>
+                <v-row dense>
+                <v-col
+                    v-for="card in cards"
+                    :key="card.title"
+                    :cols="card.flex"
                 >
-                <v-icon
-                    large
-                    color="darken-2"
-                >
-                mdi-magnify
-                </v-icon>
-            </v-btn>
-        </div>
+                    <v-card>
+                    <v-img
+                        :src="card.src"
+                        class="white--text align-end"
+                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                        height="200px"
+                    >
+                        <v-card-title style="font-size: medium;">{{ card.title }}</v-card-title>
+                    </v-img>
+                    
+                    <v-card-actions >
+                        <!-- 버튼 추가 할거면 여기 -->
+                        <v-col>
+                        <v-row>
+                            <v-btn icon>
+                            <v-icon color="black">mdi-map-search</v-icon>
+                            </v-btn>
+                        </v-row>
+                        <v-row>
+                            <v-btn icon @click="card.like = !card.like">
+                            <v-icon :color="card.like ? 'red' : '' ">mdi-heart</v-icon>
+                            </v-btn>
+                        </v-row>
+                        </v-col>
+                        <v-spacer>{{ card.addr_1 }}</v-spacer>
+                        <v-btn
+                        icon
+                        @click="card.show = !card.show"
+                        >
+                        <v-icon>{{ card.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                    <v-expand-transition>
+                        <div v-show="card.show">
+                        <v-divider></v-divider>
+                        <v-card-text>{{ card.overview }}</v-card-text>
+                        </div>
+                    </v-expand-transition>
+                    </v-card>
+                </v-col>
+                </v-row>
+            </v-container>
 
-        <div class="mt-3 me-3">
-			<div id="map" class="" style="width: 100%; height: 700px;"></div>
-		</div>
-
-        <div>
-            {{ selectedContentById }}
-        </div>
-    </v-app>
-
-
+            </v-sheet>
+            </v-col>
+        </v-row>
+    </v-container>
+    </v-main>
+</v-app>
 </template>
 
 
@@ -81,14 +157,19 @@ export default {
             selectedContentById: [],
             positions: [],
             markers: [],
-            contentByType: [{id: 12, value: "관광지"},
-                            { id: 14, value: "문화시설" },
-                            { id: 15, value: "축제공연행사" },
-                            { id: 25, value: "여행코스" },
-                            { id: 28, value: "레포츠" },
-                            { id: 32, value: "숙박" },
-                            { id: 38, value: "쇼핑" },
-                            { id: 39, value: "음식점" }]
+            contentByType: [
+                    { id: 12, value: "관광지" },
+                    { id: 14, value: "문화시설" },
+                    { id: 15, value: "축제공연행사" },
+                    { id: 25, value: "여행코스" },
+                    { id: 28, value: "레포츠" },
+                    { id: 32, value: "숙박" },
+                    { id: 38, value: "쇼핑" },
+                    { id: 39, value: "음식점" }
+                ],
+            cards: [],
+            overlay: [],
+            clickedOverlay: null
         };
     },
     created() {
@@ -109,16 +190,12 @@ export default {
                     this.map = new window.kakao.maps.Map(mapContainer, mapOption);
                     this.createRightBar();
                 });
-            }
-            
+            }  
         };
         script.async = true;
         script.type = 'text/javascript';
         script.src = "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=1b7a0eb6294cdd4b1f985683a25bd972";
         document.head.appendChild(script);
-        
-
-    
     },
     watch: {
         selectedSido: "create_gugun"
@@ -153,20 +230,36 @@ export default {
         markList() {
             http.get(`/tour/attraction-info?search-area=${this.selectedSido}&search-area-gu=${this.selectedGugun}`)
                 .then((response) => {
+                    this.positions.length = 0;
+                    this.cards.length = 0;
                     // console.log(response.data);
                     response.data.forEach((area) => {
-                        let markerInfo = {
-                            id: area.contentId,
-                            img: area.firstImage,
-                            title: area.title,
-                            addr_1: area.addr1,
-                            addr_2: area.addr2,
-                            zip: area.zipcode,
-                            tel: area.tel,
-                            contenttypeid: area.contentTypeId,
-                            latlng: new window.kakao.maps.LatLng(area.latitude, area.longitude),
-                        };
-                        this.positions.push(markerInfo);
+                        // 체크된것만!
+                        if (this.selectedContentById.includes(area.contentTypeId)){
+                            let markerInfo = {
+                                id: area.contentId,
+                                img: area.firstImage,
+                                title: area.title,
+                                addr_1: area.addr1,
+                                addr_2: area.addr2,
+                                zip: area.zipcode,
+                                tel: area.tel,
+                                contenttypeid: area.contentTypeId,
+                                latlng: new window.kakao.maps.LatLng(area.latitude, area.longitude),
+                            };
+                            let card = {
+                                id: area.contentId,
+                                src: area.firstImage,
+                                title: area.title,
+                                addr_1: area.addr1,
+                                overview: area.overView,
+                                flex: 3,
+                                show: false,
+                                like: false
+                            }
+                            this.positions.push(markerInfo);
+                            this.cards.push(card);
+                        }
                     });
                 })
                 .then(() => { 
@@ -176,13 +269,9 @@ export default {
         },
         displayMarker() {
             var imageSrc = "";
-            let index = 0;
+            // let index = 0;
             // 마커 이미지
             for (var i = 0; i < this.positions.length; i++) {
-                if(!this.selectedContentById.includes(this.positions[i].contenttypeid)){
-                    continue;
-                }
-
                 if (this.positions[i].contenttypeid == 12) {
                     imageSrc = require("@/assets/mark/mark1.png");
                 }else if(this.positions[i].contenttypeid == 14){
@@ -201,7 +290,6 @@ export default {
                     imageSrc = require("@/assets/mark/mark8.png");
                 }
         
-
                 // 마커 이미지의 이미지 크기 입니다
                 /*var imageSize = new kakao.maps.Size(100, 80);*/
                 var imageSize = new window.kakao.maps.Size(100, 69); // 마커이미지의 크기입니다
@@ -210,7 +298,7 @@ export default {
                 var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
 
                 // 마커를 생성합니다
-                this.markers[index++] = new window.kakao.maps.Marker({
+                this.markers[i] = new window.kakao.maps.Marker({
                     map: this.map, // 마커를 표시할 지도
                     position: this.positions[i].latlng, // 마커를 표시할 위치
                     title: this.positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
@@ -218,7 +306,51 @@ export default {
                 });
             }
             // 첫번째 검색 정보를 이용하여 지도 중심을 이동 시킵니다
-            this.map.setCenter(this.positions[0].latlng);
+            if(this.positions.length) this.map.setCenter(this.positions[0].latlng);
+
+             // 마커를 클릭하면 오버레이를 띄운다
+            for (let i = 0; i < this.markers.length; i++){
+                window.kakao.maps.event.addListener(this.markers[i], 'click', () => {
+                    // let display = "";
+                    // 좋아요 저장할 때 쓸 꺼
+                    // temp_id = this.positions[i].id;
+                    // if(myLocationlist.includes(this.positions[i].id)){
+                    //     display = "none";
+                    // }
+                    var content = '<v-hover>' + 
+                    '      <v-card color="grey lighten-4" flat height="200px" tile>' + 
+                    `           ${this.positions[i].title}` + 
+                    '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' + 
+                    '        </v-card>' + 
+                    '        <v-col class="body">' + 
+                    '            <div class="img">' +
+                    `                <img src="${this.positions[i].img}" onerror="this.src='/assets/img/mark/noimg.png'" width="73" height="70">` +
+                    '           </div>' + 
+                    '            <div class="desc">' + 
+                    `                <div class="ellipsis">${this.positions[i].addr_1}</div>` + 
+                    `                <div class="jibun ellipsis">(우) ${this.positions[i].zip}</br>(전화번호) ${this.positions[i].tel}</div>` + 
+                    '            </div>' + 
+                    '        </v-col>' +   
+                    '</v-hover>';
+                
+                
+                    this.overlay = new window.kakao.maps.CustomOverlay({
+                        content: content,
+                        map: this.map,
+                        position: this.positions[i].latlng,   
+                    });
+                    
+                    // 마커 클릭 시 부드럽게 가운데로 이동시키기 
+                    if(this.clickedOverlay){
+                        this.clickedOverlay.setMap(null);
+                    }
+                    this.overlay.setMap(this.map);
+                    this.clickedOverlay = this.overlay;
+                    
+                    this.map.panTo(this.overlay.getPosition());
+                });
+            }	
+
         },
         removeMarker() {
             for ( var i = 0; i < this.markers.length; i++ ) {
@@ -233,4 +365,5 @@ export default {
 </script>
 
 
-<style scoped></style>
+<style scoped>
+</style>
