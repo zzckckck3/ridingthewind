6c029bad1df65ad190b9df709e30d1d00f2bdab6
yuhojae <ho_0214@naver.com>
2023-05-19 16:06:57 +0900

    feat yuhojae filter add temp

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,30 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+// import store from "@/store";
 Vue.use(VueRouter);
+
+// const onlyAuthUser = async (to, from, next) => {
+//   const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+//   const checkToken = store.getters["memberStore/checkToken"];
+//   let token = sessionStorage.getItem("access-token");
+//   console.log("로그인 처리 전", checkUserInfo, token);
+//
+//   alert("들어왔어요");
+//
+//   if (checkUserInfo != null && token) {
+//     console.log("토큰 유효성 체크하러 가자!!!!");
+//     await store.dispatch("memberStore/getUserInfo", token);
+//   }
+//   if (!checkToken || checkUserInfo === null) {
+//     alert("로그인이 필요한 페이지입니다..");
+//     // next({ name: "login" });
+//     router.push({ name: "signin" });
+//   } else {
+//     console.log("로그인 했다!!!!!!!!!!!!!.");
+//     next();
+//   }
+// };
 
 const routes = [
   {
@@ -79,6 +102,7 @@ const routes = [
   {
     path: "/mypage",
     name: "mypage",
+    // beforeEnter: onlyAuthUser,
     component: () => import(/* webpackChunkName: "trip" */ "@/views/PersonalTripView.vue"),
     children: [],
   },
@@ -89,5 +113,6 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
 
 export default router;
