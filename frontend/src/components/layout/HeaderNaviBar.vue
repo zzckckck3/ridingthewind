110b8ed2496971085b90d0e6b1e474270ab25dc2
유호재 <ho_0214@naver.com>
2023-05-20 16:45:34 +0900

    feat yuhojae modal nickname update, session storage role add complete

--- frontend/src/components/layout/HeaderNaviBar.vue
+++ frontend/src/components/layout/HeaderNaviBar.vue
@@ -29,85 +29,85 @@
 
         <div class="text-center">
             <v-menu
-            v-model="menu"
-            :close-on-content-click="false"
-            :nudge-width="200"
-            offset-x
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-width="200"
+                offset-x
             >
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                color="blue lighten-1 text-capitalize"
-                dark
-                v-bind="attrs"
-                v-on="on"
-                >
-                UserId
-                </v-btn>
-            </template>
-
-            <v-card>
-                <v-list>
-                <v-list-item>
-                    <v-list-item-avatar>
-                    <img
-                        src="https://cdn.vuetifyjs.com/images/john.jpg"
-                        alt=""
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        color="blue lighten-1 text-capitalize"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
                     >
-                    </v-list-item-avatar>
+                        {{ isLogin ? userInfo.data.memberId : 'Login' }}
+                    </v-btn>
+                </template>
 
-                    <v-list-item-content>
-                        <v-list-item-title>Name</v-list-item-title>
-                    
-                        <v-list-item-subtitle>ID or Email</v-list-item-subtitle>
-                    </v-list-item-content>
+                <v-card>
+                    <v-list>
+                        <v-list-item>
+                            <v-list-item-avatar>
+                                <img
+                                    src="https://cdn.vuetifyjs.com/images/john.jpg"
+                                    alt=""
+                                >
+                            </v-list-item-avatar>
 
-                </v-list-item>
-                </v-list>
+                            <v-list-item-content>
+                                <v-list-item-title> {{ isLogin ? userInfo.data.memberId : 'name' }}</v-list-item-title>
 
-                <v-divider></v-divider>
+                                <v-list-item-subtitle>{{ isLogin ? userInfo.data.memberId : 'userId' }}</v-list-item-subtitle>
+                            </v-list-item-content>
 
-                <v-list rounded>
-                    <v-subheader>REPORTS</v-subheader>
-                    <v-list-item-group
-                        v-model="selectedItem"
-                        color="primary"
-                    >
-                        <v-list-item
-                        v-for="(item, i) in items"
-                        :key="i"
-                        >
-                        <v-list-item-icon>
-                            <v-icon>{{item.icon}}</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>{{ item.text }}</v-list-item-title>
-                        </v-list-item-content>
                         </v-list-item>
-                    </v-list-item-group>
                     </v-list>
 
-                <v-card-actions>
-                    <v-spacer></v-spacer>
+                    <v-divider></v-divider>
 
-                    <v-btn
-                        text
-                        @click="menu = false"
-                    >
-                        Cancel
-                    </v-btn>
-                    <v-btn
-                        color="primary"
-                        text
-                        @click="menu = false"
-                    >
-                        Save
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
+                    <v-list rounded>
+                        <v-subheader>REPORTS</v-subheader>
+                        <v-list-item-group
+                            v-model="selectedItem"
+                            color="primary"
+                        >
+                            <v-list-item
+                                v-for="(item, i) in items"
+                                :key="i"
+                            >
+                                <v-list-item-icon>
+                                    <v-icon>{{item.icon}}</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ item.text }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+
+                        <v-btn
+                            text
+                            @click="menu = false"
+                        >
+                            Cancel
+                        </v-btn>
+                        <v-btn
+                            color="primary"
+                            text
+                            @click="menu = false"
+                        >
+                            Save
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-menu>
         </div>
     </v-app-bar>
-</template> 
+</template>
 
 <script>
 import { mapState, mapActions, mapGetters } from "vuex";
