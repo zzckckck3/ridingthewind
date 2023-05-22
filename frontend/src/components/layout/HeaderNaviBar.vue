8cd7b3604853a22bec5d2b58385f0b1911acfa09
나종현 <i0364842@naver.com>
2023-05-22 15:12:29 +0900

    오류수정2

--- frontend/src/components/layout/HeaderNaviBar.vue
+++ frontend/src/components/layout/HeaderNaviBar.vue
@@ -106,7 +106,7 @@
                 </v-card>
             </v-menu>
             <member-login ref="loginOverlay" @showSignup="openSignupModal"></member-login>
-            <member-signup ref="signupOverlay"></member-signup>
+            <member-signup ref="signupOverlay" @showLogin="openLoginModal"></member-signup>
         </div>
     </v-app-bar>
 </template>
