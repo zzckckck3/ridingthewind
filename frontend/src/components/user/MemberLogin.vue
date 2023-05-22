c4a747e48eb8ebbf3866a5c9de624e51118f8686
배용현 <byh9811@naver.com>
2023-05-22 15:42:12 +0900

    FIX: FIx tripdelete while writing article
    1. 게시글 작성시 여행 장소가 삭제되지 않는 버그 수정
    2. 깃이 꼬였는지 오타가 생겼길래 수정

--- frontend/src/components/user/MemberLogin.vue
+++ frontend/src/components/user/MemberLogin.vue
@@ -86,10 +86,6 @@ export default {
             } else {
                 alert("아이디와 비밀번호를 확인해주세요");
             }
-
-          }else{
-              alert("아이디와 비밀번호를 확인해주세요");
-          }
       },
       addUserShow() {
         this.loginModal = false;
