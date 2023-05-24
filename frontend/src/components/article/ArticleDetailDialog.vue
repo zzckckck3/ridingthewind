be18866b34025c6d0b20f4fafdc72c009583d98e
zzckckck3 <zzckckck3@naver.com>
2023-05-24 18:01:09 +0900

    adding dialog

---
+++ frontend/src/components/article/ArticleDetailDialog.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-dialog v-model="dialog" max-width="1200">
+    <v-container>
+      <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="10">
+          <h1>공유 게시판</h1>
+          <v-divider class="my-2"></v-divider>
+          <!-- 나머지 내용 생략 -->
+        </v-col>
+        <v-col cols="1"></v-col>
+      </v-row>
+    </v-container>
+  </v-dialog>
+</template>
+
+<script>
+import http from "@/axios/http.js";
+import { mapState } from "vuex";
+const memberStore = "memberStore";
+import DeleteConfirmDialog from "@/components/layout/DeleteConfirmDialog.vue";
+import CommentDeleteConfirmDialog from "@/components/layout/CommentDeleteConfirmDialog.vue";
+
+export default {
+  name: "ArticleDetailDialog",
+  data() {
+    return {
+      article: {},
+      cards: [],
+      comment: "",
+      dialog: false, // 다이얼로그 열림/닫힘 상태
+      commentDialog: false,
+      deleteCommentNo: 0,
+    };
+  },
+  created() {
+    this.getArticle();
+  },
+  computed: {
+    ...mapState(memberStore, ["userInfo"]),
+  },
+  methods: {
+    getArticle() {
+      let articleNo = this.$route.params.articleNo;
+      http.get(`/article/${articleNo}`)
+        .then(({ data }) => {
+          this.article = data;
+          this.comment = "";
+          this.cards = [];
+          data.tourList.forEach((area) => {
+            let card = {
+              id: area.contentId,
+              src: area.firstImage,
+              title: area.title,
+              addr_1: area.addr1,
+              overview: area.overView,
+              latitude: area.latitude,
+              longitude: area.longitude,
+              flex: 12,
+              show: false,
+              like: true,
+            };
+            this.cards.push(card);
+          });
+        })
+        .catch((error) => {
+          this.$router.push("error/error", error);
+        });
+    },
+    moveToList() {
+      this.$router.push({ name: "article" });
+    },
+    moveToModify() {
+      this.$router.push({
+        name: "articleModify",
+        params: {
+          article: JSON.stringify(this.article),
+          cards: JSON.stringify(this.cards),
+        },
+      });
+    },
+    addComment() {
+      let newComment = {
+        articleNo: this.article.articleNo,
+        memberId: this.userInfo.data.memberId,
+        content: this.comment,
+      };
+
+      http.post(`/comment`, JSON.stringify(newComment))
+        .then(({ data }) => {
+          this.article = data;
+          this.getArticle();
+        })
+        .catch((error) => {
+          this.$router.push("/error/error", error);
+        });
+    },
+    deleteComment(key) {
+      this.commentDialog = true;
+      this.deleteCommentNo = key;
+    },
+  },
+  components: {
+    CommentDeleteConfirmDialog,
+    DeleteConfirmDialog,
+  },
+};
+</script>
--- frontend/src/components/article/ArticleList.vue
+++ frontend/src/components/article/ArticleList.vue
@@ -2,104 +2,106 @@
     <v-container>
         <v-row>
             <v-col cols="1"></v-col>
+            
             <v-col cols="10">
-            <h1>공유 게시판</h1>
-            <v-divider class="my-2"></v-divider>
-        <v-row class="mt-3">
-            <v-col class="d-flex" cols="5">
-                <v-col cols="3">
-                    <v-select
-                        :items="keyList"
-                        value="`${key}`"
-                        label="검색 조건"
-                        dense
-                        outlined
-                        @input="changeKey"
-                    ></v-select>
+                <h1>공유 게시판</h1>
+                <v-divider class="my-2"></v-divider>
+            
+            <v-row class="mt-3">
+                <v-col class="d-flex" cols="5">
+                    <v-col cols="3">
+                        <v-select
+                            :items="keyList"
+                            value="`${key}`"
+                            label="검색 조건"
+                            dense
+                            outlined
+                            @input="changeKey"
+                        ></v-select>
+                    </v-col>
+                    <v-col cols="5">
+                        <v-text-field v-model="word" outlined dense></v-text-field>
+                    </v-col>
+                    <v-col cols="1" style="padding-left: 0;">
+                        <v-btn color="indigo" elevation="3" @click="getArticleList"
+                            ><v-icon class="mt-1" color="white">mdi-magnify</v-icon></v-btn
+                        >
+                    </v-col>
                 </v-col>
-                <v-col cols="5">
-                    <v-text-field v-model="word" outlined dense></v-text-field>
+                <v-col class="d-flex flex-row" style="align-items: center;" offset="2" cols="5">
+                    <v-col cols="6"></v-col>
+                    <v-col cols="2" class="p-0 custom-margin-top">
+                        <v-select
+                            style="width: 80%;"
+                            :items="sppList"
+                            value="`${spp}`"
+                            label="표시"
+                            dense
+                            outlined
+                            @input="changeSpp"
+                        ></v-select>
+                    </v-col>
+                    <v-col cols="3" class="p-0" >
+                        <v-btn
+                            color="blue"
+                            elevation="3"
+                            plain
+                            @click="$router.push({ name: 'plan'})"
+                        >여행 경로 공유</v-btn>
+                    </v-col>
+                    <v-col cols="1" class="p-0"> 
+                        <v-btn
+                        class="mx-2"
+                        fab
+                        dark
+                        large
+                        color="indigo"
+                        @click="moveToArticleWrite"
+                        >
+                        <v-icon dark>
+                            mdi-pencil
+                        </v-icon>
+                        </v-btn>
+                    </v-col>
                 </v-col>
-                <v-col cols="1" style="padding-left: 0;">
-                    <v-btn color="indigo" elevation="3" @click="getArticleList"
-                        ><v-icon class="mt-1" color="white">mdi-magnify</v-icon></v-btn
+            </v-row>
+            <v-simple-table class="mt-3">
+                <thead>
+                    <tr>
+                        <th
+                            v-for="(header, idx) in headers"
+                            :key="idx"
+                            v-text="header"
+                            class="text-h6 text-center"
+                        ></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="article in articles"
+                        :key="article.articleNo"
+                        @click="moveToArticleDetail(article.articleNo)"
                     >
-                </v-col>
-            </v-col>
-            <v-col class="d-flex flex-row" style="align-items: center;" offset="2" cols="5">
-                <v-col cols="6"></v-col>
-                <v-col cols="2" class="p-0 custom-margin-top">
-                    <v-select
-                        style="width: 80%;"
-                        :items="sppList"
-                        value="`${spp}`"
-                        label="표시"
-                        dense
-                        outlined
-                        @input="changeSpp"
-                    ></v-select>
-                </v-col>
-                <v-col cols="3" class="p-0" >
-                    <v-btn
-                        color="blue"
-                        elevation="3"
-                        plain
-                        @click="$router.push({ name: 'plan'})"
-                    >여행 경로 공유</v-btn>
-                </v-col>
-                <v-col cols="1" class="p-0"> 
-                    <v-btn
-                    class="mx-2"
-                    fab
-                    dark
-                    large
+                        <td>{{ article.articleNo }}</td>
+                        <td>{{ article.like }}</td>
+                        <td>{{ article.subject }}</td>
+                        <td>{{ article.nickname }}</td>
+                        <td>{{ article.hit }}</td>
+                        <td>{{ article.registerTime }}</td>
+                    </tr>
+                </tbody>
+            </v-simple-table>
+            <div class="text-center">
+                <v-pagination
+                    v-model="curPage"
+                    :length="maxPage"
+                    :total-visible="naviSize"
                     color="indigo"
-                    @click="moveToArticleWrite"
-                    >
-                    <v-icon dark>
-                        mdi-pencil
-                    </v-icon>
-                    </v-btn>
-                </v-col>
-            </v-col>
-        </v-row>
-        <v-simple-table class="mt-3">
-            <thead>
-                <tr>
-                    <th
-                        v-for="(header, idx) in headers"
-                        :key="idx"
-                        v-text="header"
-                        class="text-h6 text-center"
-                    ></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr
-                    v-for="article in articles"
-                    :key="article.articleNo"
-                    @click="moveToArticleDetail(article.articleNo)"
-                >
-                    <td>{{ article.articleNo }}</td>
-                    <td>{{ article.like }}</td>
-                    <td>{{ article.subject }}</td>
-                    <td>{{ article.nickname }}</td>
-                    <td>{{ article.hit }}</td>
-                    <td>{{ article.registerTime }}</td>
-                </tr>
-            </tbody>
-        </v-simple-table>
-        <div class="text-center">
-            <v-pagination
-                v-model="curPage"
-                :length="maxPage"
-                :total-visible="naviSize"
-                color="indigo"
-                dark
-            ></v-pagination>
-        </div>
-    </v-col>
-    <v-col cols="1"></v-col>
+                    dark
+                ></v-pagination>
+            </div>
+        </v-col>
+        <v-col cols="1"></v-col>
     </v-row>
     </v-container>
 </template>
@@ -126,6 +128,7 @@ export default {
             naviSize: 1,
             key: "",
             word: "",
+            interDialog: false,
         };
     },
     created() {
